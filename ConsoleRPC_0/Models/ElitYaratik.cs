690ea9a1b7843acb88a429861f0e8142cec673e7
Beyza Uzungel <129589424+beyza-uzungel@users.noreply.github.com>
2024-12-08 19:51:16 +0300

    Add all files

---
+++ ConsoleRPC_0/Models/ElitYaratik.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_0.Models
+{
+    internal class ElitYaratik : KarakterYaratikSpec
+    {
+        public override void OzelSaldiri()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleRPC_0/Models/Sehir.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_0.Models
+{
+    internal class Sehir : BaseEntity
+    {
+        public List<Irk> Irklari { get; set; }
+        public Bolge Bolge { get; set; }
+
+
+    }
+}
